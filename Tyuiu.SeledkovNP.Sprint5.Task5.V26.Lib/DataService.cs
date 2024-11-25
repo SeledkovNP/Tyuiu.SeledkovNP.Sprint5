f76e1e1c56773d788b8c87fd4321d91f8016497b
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-11-25 11:09:33 +0500

    Task5

---
+++ Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib
+{
+    public class DataService : ISprint5Task4V26
+    {
+        public double LoadFromDataFile(string path)
+        {
+            
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    res = res + Convert.ToDouble(line);
+                }
+               
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.SeledkovNP.Sprint5.Task5.V26/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib;
+namespace Tyuiu.SeledkovNP.Sprint5.Task5.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Селедков.Н.П | Смартб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набор данных из текстового файла                           *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Селедков Никита Павлович | Смартб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти разницу между суммой всех положительных и отрицательных           *");
+            Console.WriteLine("* вещественных чисел в файле                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask5.txt");
+            // поиск файла OutPutFileTask5.txt
+            Console.WriteLine("Данные находяться в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Сумма всех эллементов: " + res);
+            Console.ReadLine();
+        }
+    }
+}
