1b161904e9db167e7f30e17b03c738ad162c8260
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-11-25 11:26:43 +0500

    Task5

--- Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib/DataService.cs
+++ Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib/DataService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
+using System.Globalization;
 namespace Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib
 {
     public class DataService : ISprint5Task5V26
@@ -18,9 +19,23 @@ namespace Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    res = res + Convert.ToDouble(line);
-                }
+                    
+                    string[] numbers = line.Split(' ');             // Разделяем строку на части по пробелам
 
+                    foreach (string number in numbers)
+                    {
+                             // Мой спаситель, который из точки сделает запетую
+                        if (double.TryParse(number, NumberStyles.Any, CultureInfo.InvariantCulture, out double value))               // Преобразуем каждую часть в double и добавляем к результату
+                        {
+                            res += value;
+                        }
+                        else
+                        {
+                            
+                            Console.WriteLine($"Не удалось преобразовать '{number}' в double.");           // Обработка случая, если преобразование не удалось 
+                        }
+                    }
+                }
             }
             return Math.Round(res, 3);
 
