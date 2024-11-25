c9d5f2aaa52a5eee85ac6670b7f19ee245d4c7ea
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-11-25 11:10:44 +0500

    Task5

--- Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib/DataService.cs
+++ Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib/DataService.cs
@@ -7,11 +7,11 @@ using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
 namespace Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib
 {
-    public class DataService : ISprint5Task4V26
+    public class DataService : ISprint5Task5V26
     {
         public double LoadFromDataFile(string path)
         {
-            
+
             double res = 0;
             using (StreamReader reader = new StreamReader(path))
             {
@@ -20,9 +20,10 @@ namespace Tyuiu.SeledkovNP.Sprint5.Task5.V26.Lib
                 {
                     res = res + Convert.ToDouble(line);
                 }
-               
+
             }
             return Math.Round(res, 3);
+
         }
     }
 }
