dfa902b94b22afc24453573d8a21bf6fdde5824b
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-11-26 18:01:37 +0500

    Task7

--- Tyuiu.SeledkovNP.Sprint5.Task7.V28.Lib/DataService.cs
+++ Tyuiu.SeledkovNP.Sprint5.Task7.V28.Lib/DataService.cs
@@ -7,41 +7,25 @@ using System.IO;
 using System.Globalization;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
 namespace Tyuiu.SeledkovNP.Sprint5.Task7.V28.Lib
 {
     public class DataService : ISprint5Task7V28
     {
         public string LoadDataAndSave(string path)
         {
-            string paths = Path.Combine(Path.GetTempPath(), "OutPutFileTask7V28.txt");
+
+            
+            string outputPath = "OutPutDataFileTask7V28.txt";                  // Определяем имя выходного файла
+
+            string inputData = File.ReadAllText(path);                       // Читаем все строки из входного файла
+
+            string cleanedData = Regex.Replace(inputData, @"s{2,}", " ");  // Используем регулярное выражение для замены пробелов
+
+            File.WriteAllText(outputPath, cleanedData);                  // Сохраняем результат в выходной файл
 
 
-            string pathSaveFile = $@"C:\DataSprint5\OutPutDataFileTask7V28.txt";
-
-            FileInfo fileInfo = new FileInfo(pathSaveFile);
-            bool fileExists = fileInfo.Exists;
-
-            if (fileExists)
-            {
-                File.Delete(pathSaveFile);
-            }
-            string strLine = "";
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    for (int i = 0; i < line.Length; i++)
-                    {
-                        strLine = strLine.Replace("  ", " ") + line[i];
-                    }
-
-                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
-                    strLine = "";
-                }
-            }
-
-            return pathSaveFile;
+            return outputPath;
 
         }
     }
