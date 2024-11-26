43ba4bbcca14cd8641de39840bfddd39abddb3aa
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-11-26 20:47:55 +0500

    Hint

--- Tyuiu.SeledkovNP.Sprint5.Task7.V28.Lib/DataService.cs
+++ Tyuiu.SeledkovNP.Sprint5.Task7.V28.Lib/DataService.cs
@@ -45,7 +45,7 @@ namespace Tyuiu.SeledkovNP.Sprint5.Task7.V28.Lib
            // {
            //    Console.WriteLine("Произошла ошибка: " + ex.Message);
            // }
-            return outputData;
+            return outputFilePath;
             //return "Это строка с пробелами";
         }
     }
