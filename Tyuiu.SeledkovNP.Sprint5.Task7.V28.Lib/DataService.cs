bda942fcf88006579ad9c9111fa60e5311583f01
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-11-26 18:59:12 +0500

    Fix

--- Tyuiu.SeledkovNP.Sprint5.Task7.V28.Lib/DataService.cs
+++ Tyuiu.SeledkovNP.Sprint5.Task7.V28.Lib/DataService.cs
@@ -20,7 +20,11 @@ namespace Tyuiu.SeledkovNP.Sprint5.Task7.V28.Lib
 
             string inputData = File.ReadAllText(path);                       // Читаем все строки из входного файла
 
-            string cleanedData = Regex.Replace(inputData, @"\s{2,}", " ");  // замены пробелов
+
+            string cleanedData = Regex.Replace(inputData, @"s{2,}", " "); 
+            //string cleanedData = path.Replace(inputData, @"\s{2,}", ' ');  // замены пробелов
+
+            //string cleanedData = path.Replace( '  ', ' ');
 
             File.WriteAllText(outputPath, cleanedData);                  // Сохраняем результат в выходной файл
 
